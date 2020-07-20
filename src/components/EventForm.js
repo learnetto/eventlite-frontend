b99adfc3b6ede27853494d22b672280e5a17acf1
Hrishi Mittal <hrishimittal@gmail.com>
2020-07-20 12:12:10 +0100

    Move all event code from Eventlite to EventForm

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,17 +1,101 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import FormErrors from './FormErrors'
+import validations from '../validations'
+import axios from 'axios'
 
-const EventForm = (props) => (
-  <div>
-    <h4>Create an Event:</h4>
-    <form onSubmit={props.handleSubmit}>
-      <input type="text" name="title" placeholder="Title" value={props.title} onChange={props.handleInput} />
-      <input type="text" name="start_datetime" placeholder="Date" value={props.start_datetime} onChange={props.handleInput} />
-      <input type="text" name="location" placeholder="Location" value={props.location} onChange={props.handleInput} />
-      <input type="submit" value="Create Event"
-       disabled={!props.formValid} />
-    </form>
-  </div>
-)
+class EventForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: {value: '', valid: false},
+      start_datetime: {value: '', valid: false},
+      location: {value: '', valid: false},
+      formErrors: {},
+      formValid: false
+    }
+  }
+
+  static formValidations = {
+    title: [
+      (value) => { return(validations.checkMinLength(value, 3)) }
+    ],
+    start_datetime: [
+      (value) => { return(validations.checkMinLength(value, 1)) },
+      (value) => { return(validations.timeShouldBeInTheFuture(value)) }
+    ],
+    location: [
+      (value) => { return(validations.checkMinLength(value, 1)) }
+    ]
+  }
+
+  handleInput = e => {
+    e.preventDefault()
+    const name = e.target.name
+    const value = e.target.value
+    const newState = {}
+    newState[name] = {...this.state[name], value: value}
+    this.setState(newState, () => this.validateField(name, value, EventForm.formValidations[name]))
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    let newEvent = { title: this.state.title.value, start_datetime: this.state.start_datetime.value, location: this.state.location.value }
+    axios({
+      method: 'POST',
+      url: 'http://localhost:3001/events',
+      headers: JSON.parse(localStorage.user),
+      data: { event: newEvent }
+    })
+    .then(response => {
+      this.addNewEvent(response.data)
+      this.resetFormErrors();
+    })
+    .catch(error => {
+      this.setState({formErrors: error.response.data})
+    })
+  }
+
+  validateField(fieldName, fieldValue, fieldValidations) {
+    let fieldValid = true
+    let errors = fieldValidations.reduce((errors, validation) => {
+      let [valid, fieldError] = validation(fieldValue)
+      if(!valid) {
+        errors = errors.concat([fieldError])
+      }
+      return(errors);
+    }, []);
+
+    fieldValid = errors.length === 0
+
+    const newState = {formErrors: {...this.state.formErrors, [fieldName]: errors}}
+    newState[fieldName] = {...this.state[fieldName], valid: fieldValid}
+    this.setState(newState, this.validateForm)
+  }
+
+  validateForm() {
+    this.setState({formValid: this.state.title.valid && this.state.location.valid && this.state.start_datetime.valid})
+  }
+
+  resetFormErrors () {
+    this.setState({formErrors: {}})
+  }
+
+  render() {
+    return (
+      <div>
+        <h4>Create an Event:</h4>
+        <FormErrors formErrors = {this.state.formErrors} />
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" name="title" placeholder="Title" value={this.state.title.value} onChange={this.handleInput} />
+          <input type="text" name="start_datetime" placeholder="Date" value={this.state.start_datetime.value} onChange={this.handleInput} />
+          <input type="text" name="location" placeholder="Location" value={this.state.location.value} onChange={this.handleInput} />
+          <input type="submit" value="Create Event"
+           disabled={!this.state.formValid} />
+        </form>
+      </div>
+    )
+  }
+}
 
 export default EventForm
--- src/components/Eventlite.js
+++ src/components/Eventlite.js
@@ -1,10 +1,7 @@
 import React from 'react'
 import axios from 'axios'
-import validations from '../validations'
-
 import EventsList from './EventsList'
 import EventForm from './EventForm'
-import FormErrors from './FormErrors'
 
 import './Eventlite.css'
 
@@ -12,26 +9,8 @@ class Eventlite extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      events: [],
-      title: {value: '', valid: false},
-      start_datetime: {value: '', valid: false},
-      location: {value: '', valid: false},
-      formErrors: {},
-      formValid: false
+      events: []
     }
-  }
-
-  static formValidations = {
-    title: [
-      (value) => { return(validations.checkMinLength(value, 3)) }
-    ],
-    start_datetime: [
-      (value) => { return(validations.checkMinLength(value, 1)) },
-      (value) => { return(validations.timeShouldBeInTheFuture(value)) }
-    ],
-    location: [
-      (value) => { return(validations.checkMinLength(value, 1)) }
-    ]
   }
 
   componentDidMount() {
@@ -42,59 +21,6 @@ class Eventlite extends React.Component {
     .then(response => {
       this.setState({events: response.data})
     })
-  }
-
-  handleInput = e => {
-    e.preventDefault()
-    const name = e.target.name
-    const value = e.target.value
-    const newState = {}
-    newState[name] = {...this.state[name], value: value}
-    this.setState(newState, () => this.validateField(name, value, Eventlite.formValidations[name]))
-  }
-
-  handleSubmit = e => {
-    e.preventDefault()
-    let newEvent = { title: this.state.title.value, start_datetime: this.state.start_datetime.value, location: this.state.location.value }
-    axios({
-      method: 'POST',
-      url: 'http://localhost:3001/events',
-      headers: JSON.parse(localStorage.user),
-      data: { event: newEvent }
-    })
-    .then(response => {
-      this.addNewEvent(response.data)
-      this.resetFormErrors();
-    })
-    .catch(error => {
-      console.log(error.response.data)
-      this.setState({formErrors: error.response.data})
-    })
-  }
-
-  validateField(fieldName, fieldValue, fieldValidations) {
-    let fieldValid = true
-    let errors = fieldValidations.reduce((errors, validation) => {
-      let [valid, fieldError] = validation(fieldValue)
-      if(!valid) {
-        errors = errors.concat([fieldError])
-      }
-      return(errors);
-    }, []);
-
-    fieldValid = errors.length === 0
-
-    const newState = {formErrors: {...this.state.formErrors, [fieldName]: errors}}
-    newState[fieldName] = {...this.state[fieldName], valid: fieldValid}
-    this.setState(newState, this.validateForm)
-  }
-
-  validateForm() {
-    this.setState({formValid: this.state.title.valid && this.state.location.valid && this.state.start_datetime.valid})
-  }
-
-  resetFormErrors () {
-    this.setState({formErrors: {}})
   }
 
   addNewEvent = (event) => {
@@ -108,14 +34,8 @@ class Eventlite extends React.Component {
     const currentUser = localStorage.getItem('user')
     return (
       <div>
-        <FormErrors formErrors = {this.state.formErrors} />
         {currentUser &&
-          <EventForm handleSubmit = {this.handleSubmit}
-            handleInput = {this.handleInput}
-            formValid={this.state.formValid}
-            title = {this.state.title.value}
-            start_datetime = {this.state.start_datetime.value}
-            location = {this.state.location.value} />
+          <EventForm />
         }
         <EventsList events={this.state.events} />
       </div>
