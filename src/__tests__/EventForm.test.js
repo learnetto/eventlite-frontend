db7f91de370e6c7fdee935a6309147c07d3352d6
Hrishi Mittal <hrishimittal@gmail.com>
2020-07-26 00:35:46 +0100

    Refactor EventForm tests to use beforeEach blocks for setup

--- src/__tests__/EventForm.test.js
+++ src/__tests__/EventForm.test.js
@@ -3,55 +3,52 @@ import { render, fireEvent } from "@testing-library/react"
 import EventForm from "../components/EventForm"
 import moment from 'moment'
 
+let container, submitButton, title, startDate, location
+
+beforeEach(() => {
+  container = render(<EventForm />)
+  submitButton = container.getByRole("button", { type: "submit"} )
+  title = container.getByLabelText(/Title/i)
+  startDate = container.getByLabelText(/Start Date/i)
+  location = container.getByLabelText(/Location/i)
+})
+
 test("submit button is disabled for empty form", () => {
-  const { getByRole } = render(<EventForm />)
-  const submitButton = getByRole("button", { type: "submit"} )
   expect(submitButton).toBeDisabled()
 })
 
 test("submit button is enabled if all form inputs are valid", () => {
-  const { getByRole, getByLabelText } = render(<EventForm />)
-  const submitButton = getByRole("button", { type: "submit"} )
   expect(submitButton).toBeDisabled()
-  fireEvent.input(getByLabelText(/Title/i), { target: { value: 'My Shiny Conference' } })
+  fireEvent.input(title, { target: { value: 'My Shiny Conference' } })
   expect(submitButton).toBeDisabled()
-  fireEvent.input(getByLabelText(/Start Date/i), { target: { value: moment().add(10, 'days') } })
+  fireEvent.input(startDate, { target: { value: moment().add(10, 'days') } })
   expect(submitButton).toBeDisabled()
-  fireEvent.input(getByLabelText(/Location/i), { target: { value: 'London' } })
+  fireEvent.input(location, { target: { value: 'London' } })
   expect(submitButton).toBeEnabled()
 })
 
-test("submit button is disabled if title is invalid", () => {
-  const { getByRole, getByLabelText } = render(<EventForm />)
-  const submitButton = getByRole("button", { type: "submit"} )
-  fireEvent.input(getByLabelText(/Title/i), { target: { value: 'My Shiny Conference' } })
-  fireEvent.input(getByLabelText(/Start Date/i), { target: { value: moment().add(10, 'days') } })
-  fireEvent.input(getByLabelText(/Location/i), { target: { value: 'London' } })
-  expect(submitButton).toBeEnabled()
-  fireEvent.input(getByLabelText(/Title/i), { target: { value: 'My' } })
-  expect(submitButton).toBeDisabled()
-})
+describe("single invalid inputs", () => {
+  beforeEach(() => {
+    fireEvent.input(title, { target: { value: 'My Shiny Conference' } })
+    fireEvent.input(startDate, { target: { value: moment().add(10, 'days') } })
+    fireEvent.input(location, { target: { value: 'London' } })    
+  })
 
+  test("submit button is disabled if title is invalid", () => {
+    expect(submitButton).toBeEnabled()
+    fireEvent.input(title, { target: { value: 'My' } })
+    expect(submitButton).toBeDisabled()
+  })
 
-test("submit button is disabled if start_datetime is invalid", () => {
-  const { getByRole, getByLabelText } = render(<EventForm />)
-  const submitButton = getByRole("button", { type: "submit"} )
-  fireEvent.input(getByLabelText(/Title/i), { target: { value: 'My Shiny Conference' } })
-  fireEvent.input(getByLabelText(/Start Date/i), { target: { value: moment().add(10, 'days') } })
-  fireEvent.input(getByLabelText(/Location/i), { target: { value: 'London' } })
-  expect(submitButton).toBeEnabled()
-  fireEvent.input(getByLabelText(/Start Date/i), { target: { value: moment().subtract(10, 'days') } })
-  expect(submitButton).toBeDisabled()
-})
+  test("submit button is disabled if start_datetime is invalid", () => {
+    expect(submitButton).toBeEnabled()
+    fireEvent.input(startDate, { target: { value: moment().subtract(10, 'days') } })
+    expect(submitButton).toBeDisabled()
+  })
 
-
-test("submit button is disabled if location is invalid", () => {
-  const { getByRole, getByLabelText } = render(<EventForm />)
-  const submitButton = getByRole("button", { type: "submit"} )
-  fireEvent.input(getByLabelText(/Title/i), { target: { value: 'My Shiny Conference' } })
-  fireEvent.input(getByLabelText(/Start Date/i), { target: { value: moment().add(10, 'days') } })
-  fireEvent.input(getByLabelText(/Location/i), { target: { value: 'London' } })
-  expect(submitButton).toBeEnabled()
-  fireEvent.input(getByLabelText(/Location/i), { target: { value: '' } })
-  expect(submitButton).toBeDisabled()
+  test("submit button is disabled if location is invalid", () => {
+    expect(submitButton).toBeEnabled()
+    fireEvent.input(location, { target: { value: '' } })
+    expect(submitButton).toBeDisabled()
+  })
 })
